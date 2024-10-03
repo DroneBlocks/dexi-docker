54c1e3154e48766f4ad8a42d8782c9dc79c193c1
Casey Hanner <bhanner95@gmail.com>
2024-10-02 23:07:39 -0500

    moved px4 to 15, and opt flow

---
+++ px4-slim/build.sh
@@ -0,0 +1,1 @@
+sudo docker build -t px4-slim:latest .
--- px4-slim/entrypoint.sh
+++ px4-slim/entrypoint.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
 cd /app/PX4-Autopilot
 # PX4_UXRCE_DDS_AG_IP=127.0.0.1
-HEADLESS=1 PX4_GZ_SIM_RENDER_ENGINE=ogre make px4_sitl_default gz_x500 
+
+/app/mavp2p udps:127.0.0.1:14540 tcps:0.0.0.0:5760 tcps:0.0.0.0:5761 udps:0.0.0.0:14541 --print-errors &
+
+HEADLESS=1 PX4_GZ_SIM_RENDER_ENGINE=ogre make px4_sitl gazebo_iris_opt_flow #px4_sitl_default gz_x500 
